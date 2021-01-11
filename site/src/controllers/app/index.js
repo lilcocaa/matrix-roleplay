60367c5fcc56e23cb8ecf8649a826f1966cb0c75
Leandro Macedo <leandro.dmacedo@jsl.com.br>
2021-01-11 16:39:37 -0300

    ajustes site

--- site/src/controllers/app/index.js
+++ site/src/controllers/app/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const axios = require('axios');
+
+const discordApiUsersMe = async (token) => {
+    return new Promise((resolve, reject) => {
+        const url = `https://discord.com/api/users/@me`;
+
+        const headers = {
+            'authorization': `${token.token_type} ${token.access_token}`,
+        };
+
+        axios.get(url, { headers })
+            .then(response => {
+                resolve(response.data);
+            })
+            .catch(error => {
+                reject(error.response.data);
+            });
+    });
+};
+
 router.get('/login', (req, res) => {
     res.send(`<h1>Login</h1> <p><a href="${process.env.CLIENT_LOGIN_URI}">Login aqui</a></p>`);
 });
@@ -14,9 +34,18 @@ router.use((req, res, next) => {
     next();
 });
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     console.log('req.cookies.token', req.cookies.token);
-    res.send('/app');
+    // res.send('/app');
+
+    const me = await discordApiUsersMe(req.cookies.token);
+    const meJson = JSON.stringify(me);
+    console.log('meJson', meJson);
+
+    res.json({
+        me,
+        meJson,
+    });
 });
 
 router.get('/logout', (req, res) => {
--- site/src/controllers/auth/callback.js
+++ site/src/controllers/auth/callback.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
+const knex = require('../../database/connection');
 
 const discordApiOauth2Token = async (code) => {
     return new Promise((resolve, reject) => {
         const url = `https://discord.com/api/oauth2/token`;
-        // console.log('url', url);
 
         const data = new URLSearchParams({
             client_id: process.env.CLIENT_ID,
@@ -13,43 +13,62 @@ const discordApiOauth2Token = async (code) => {
             code: code,
             scope: 'identify email guilds',
         });
-        // console.log('data', data);
 
         const headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
         };
-        // console.log('headers', headers);
 
         axios.post(url, data, { headers })
             .then(response => {
-                // console.log('then', response.data);
                 resolve(response.data);
             })
             .catch(error => {
-                // console.log('catch', error.response.data);
                 reject(error.response.data);
             });
+    });
+};
 
-        // resolve('ok');
+const discordApiUsersMe = async (token) => {
+    return new Promise((resolve, reject) => {
+        const url = `https://discord.com/api/users/@me`;
+
+        const headers = {
+            'authorization': `${token.token_type} ${token.access_token}`,
+        };
+
+        axios.get(url, { headers })
+            .then(response => {
+                resolve(response.data);
+            })
+            .catch(error => {
+                reject(error.response.data);
+            });
     });
 };
 
 module.exports = async (req, res) => {
     const ret = req.ret;
+
     if (!req.query.code) {
         return res.send('Falta code');
     }
 
     try {
         const token = await discordApiOauth2Token(req.query.code);
-        // globalToken = JSON.parse(JSON.stringify(token));
+        const me = await discordApiUsersMe(token);
+
+        await knex('discord_api_token')
+            .insert({
+                guild_id: process.env.GUILD_ID,
+                member_id: me.id,
+                token: JSON.stringify(token),
+            });
+
+        res.cookie('token', token);
 
         // return res.json({
         //     token,
-        //     // globalToken,
         // });
-
-        res.cookie('token', token);
 
         return res.redirect('/app');
     } catch (error) {

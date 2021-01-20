1a18eea4949488fd6ed2d86508a623c818ae6668
Leandro Macedo <leandro.dmacedo@jsl.com.br>
2021-01-20 11:33:16 -0300

    ajustes pagina hierarquia

--- site/src/controllers/hierarchy.js
+++ site/src/controllers/hierarchy.js
@@ -1,124 +1,107 @@
 const knex = require('../database/connection');
 const utf8 = require('utf8');
-
-async function getSquads() {
-    const query = `
-    SELECT
-        s.squad_id
-    , sn.level_id
-    , s.name AS 'squad_name'
-    , sn.name AS 'level_name'
-    , sn.salary AS 'level_salary'
-    , GROUP_CONCAT(CONCAT(m.member_id, ';;', m.nick) SEPARATOR '||') AS 'members'
-    FROM discord_squads s
-    CROSS JOIN discord_squad_level sn
-    LEFT JOIN discord_squad_members sm ON (s.squad_id = sm.squad_id AND sn.level_id = sm.level_id)
-    LEFT JOIN discord_members m ON (sm.member_id = m.member_id)
-    WHERE s.guild_id = ?
-    GROUP BY s.name, sn.level_id
-    ORDER BY s.name, sn.level_id;
-    `;
-
-    const args = ['765235242600103936'];
-
-    const queryData = (await knex.raw(query, args))[0];
-
-    const { squads, levels } = queryData.reduce((a, c) => {
-        const {
-            squad_id,
-            level_id,
-            squad_name,
-            level_name,
-            level_salary,
-            members,
-        } = c;
-
-        if (typeof a.levels[level_id] === 'undefined') {
-            a.levels[level_id] = {
-                id: level_id,
-                name: level_name,
-                salary: level_salary,
-            };
-        }
-
-        if (typeof a.squads[squad_id] === 'undefined') {
-            a.squads[squad_id] = {
-                id: squad_id,
-                name: squad_name,
-                levels: {},
-            };
-        }
-
-        if (typeof a.squads[squad_id].levels[level_id] === 'undefined') {
-            a.squads[squad_id].levels[level_id] = {
-                id: level_id,
-                name: level_name,
-                members: [],
-            };
-        }
-
-        if (members) {
-            const membersSplit = members.split('||').map(member => {
-                const m = member.split(';;')
-                m[1] = utf8.decode(m[1]);
-                return m;
-            });
-            a.squads[squad_id].levels[level_id].members = membersSplit;
-        }
-
-        return a;
-    }, {
-        squads: {},
-        levels: {},
-    });
-
-    return {
-        squads,
-        levels,
-    };
-}
-
-async function getDirectors() {
-    const query = `
-    SELECT m.member_id, m.nick
-    FROM discord_guilds g
-    INNER JOIN discord_roles r ON (g.guild_id = r.guild_id AND r.role_id = ?)
-    INNER JOIN discord_member_roles mr ON (g.guild_id = mr.guild_id AND r.role_id = mr.role_id)
-    INNER JOIN discord_members m ON (g.guild_id = m.guild_id AND mr.member_id = m.member_id)
-    WHERE g.guild_id = ?;
-    `;
-
-    const args = ['765343048401682442', '765235242600103936'];
-
-    const queryData = (await knex.raw(query, args))[0];
-
-    const members = queryData.reduce((a, c) => {
-        const {
-            member_id,
-            nick
-        } = c;
-
-        if (typeof a[member_id] === 'undefined') {
-            a[member_id] = {
-                id: member_id,
-                nick: utf8.decode(nick),
-            };
-        }
-
-        return a;
-    }, {});
-
-    return members;
-}
+const { number_format } = require('../helpers/helpers');
 
 module.exports = async (req, res) => {
 
-    const { squads, levels } = await getSquads();
-    const members = await getDirectors();
+    try {
+        const query = `
+            SELECT
+                hg.group_id
+                , hs.squad_id
+                , hl.level_id
+                , m.member_id
+                , hg.name AS group_name
+                , hs.name AS squad_name
+                , hl.name AS level_name
+                , IFNULL(hl.salary, 0) AS level_salary
+                , m.username AS member_username
+                , m.nick AS member_nick
+                , m.avatar AS member_avatar
+            FROM discord_hierarchy_groups hg
+            INNER JOIN discord_hierarchy_squads hs ON (hg.guild_id = hs.guild_id AND hg.group_id = hs.group_id)
+            INNER JOIN discord_hierarchy_squad_levels hsl ON (hs.guild_id = hsl.guild_id AND hs.squad_id = hsl.squad_id)
+            INNER JOIN discord_hierarchy_levels hl ON (hsl.guild_id = hl.guild_id AND hsl.level_id = hl.level_id)
+            LEFT JOIN discord_hierarchy_squad_members hsm ON (hs.guild_id = hsm.guild_id AND hs.squad_id = hsm.squad_id AND hl.level_id = hsm.level_id)
+            LEFT JOIN discord_members m ON (hsm.guild_id = m.guild_id AND hsm.member_id = m.member_id)
+            WHERE hg.guild_id = ?
+            ORDER BY
+                hg.order
+                , hl.order
+                , hs.order
+                , m.member_id
+            ;
+        `;
 
-    res.render('hierarchy', {
-        squads,
-        levels,
-        members,
-    });
+        const args = [process.env.GUILD_ID];
+
+        const queryData = (await knex.raw(query, args))[0];
+
+        const groups = {};
+
+        for (let i in queryData) {
+            const group_id = queryData[i].group_id;
+            const squad_id = queryData[i].squad_id;
+            const level_id = queryData[i].level_id;
+            const member_id = queryData[i].member_id;
+            const group_name = queryData[i].group_name;
+            const squad_name = queryData[i].squad_name;
+            const level_name = queryData[i].level_name;
+            const level_salary = queryData[i].level_salary;
+            const member_username = utf8.decode(queryData[i].member_username || '');
+            const member_nick = utf8.decode(queryData[i].member_nick || '');
+            const member_fullname = member_nick ? member_nick : member_username;
+            const member_avatar = queryData[i].member_avatar;
+
+            if (typeof groups[group_id] === 'undefined') {
+                groups[group_id] = {
+                    id: group_id,
+                    name: group_name,
+                    levels: {},
+                    squads: {},
+                };
+            }
+
+            if (typeof groups[group_id].levels[level_id] === 'undefined') {
+                groups[group_id].levels[level_id] = {
+                    id: level_id,
+                    name: level_name,
+                    salary: level_salary,
+                    salaryFormatted: number_format(level_salary, 0, ',', '.'),
+                };
+            }
+
+            if (typeof groups[group_id].squads[squad_id] === 'undefined') {
+                groups[group_id].squads[squad_id] = {
+                    id: squad_id,
+                    name: squad_name,
+                    levels: {},
+                };
+            }
+
+            if (member_id) {
+                if (typeof groups[group_id].squads[squad_id].levels[level_id] === 'undefined') {
+                    groups[group_id].squads[squad_id].levels[level_id] = {
+                        members: {},
+                    };
+                }
+
+                if (typeof groups[group_id].squads[squad_id].levels[level_id].members[member_id] === 'undefined') {
+                    groups[group_id].squads[squad_id].levels[level_id].members[member_id] = {
+                        id: member_id,
+                        username: member_username,
+                        nick: member_nick,
+                        fullname: member_fullname,
+                        avatar: member_avatar,
+                    };
+                }
+            }
+        }
+
+        res.render('hierarchy', {
+            groups,
+        });
+
+    } catch (error) { }
+
 };
--- site/src/helpers/helpers.js
+++ site/src/helpers/helpers.js
@@ -160,6 +160,37 @@ function hslToRgb(h, s, l) {
     };
 };
 
+function number_format(number, decimals, decPoint, thousandsSep) {
+    number = (number + '').replace(/[^0-9+\-Ee.]/g, '')
+    const n = !isFinite(+number) ? 0 : +number
+    const prec = !isFinite(+decimals) ? 0 : Math.abs(decimals)
+    const sep = (typeof thousandsSep === 'undefined') ? ',' : thousandsSep
+    const dec = (typeof decPoint === 'undefined') ? '.' : decPoint
+    let s = ''
+    const toFixedFix = function (n, prec) {
+        if (('' + n).indexOf('e') === -1) {
+            return +(Math.round(n + 'e+' + prec) + 'e-' + prec)
+        } else {
+            const arr = ('' + n).split('e')
+            let sig = ''
+            if (+arr[1] + prec > 0) {
+                sig = '+'
+            }
+            return (+(Math.round(+arr[0] + 'e' + sig + (+arr[1] + prec)) + 'e-' + prec)).toFixed(prec)
+        }
+    }
+    // @todo: for IE parseFloat(0.55).toFixed(0) = 0;
+    s = (prec ? toFixedFix(n, prec).toString() : '' + Math.round(n)).split('.')
+    if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep)
+    }
+    if ((s[1] || '').length < prec) {
+        s[1] = s[1] || ''
+        s[1] += new Array(prec - s[1].length + 1).join('0')
+    }
+    return s.join(dec)
+}
+
 module.exports = {
     htmlentities,
     intToHex,
@@ -168,4 +199,5 @@ module.exports = {
     rgbToHex,
     rgbToHsl,
     hslToRgb,
+    number_format,
 };
